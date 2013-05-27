a864a95596c53f9e01f6278ce9bddaf3fb6e75c1
Steven Barbaro <StevenEBarbaro@gmail.com>
2013-05-27 03:19:08 +0000

    adding index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var Hapi = require('hapi'),
+    serverConfig = require('./config/config').config,
+    server = new Hapi.Server(serverConfig.hostname, serverConfig.port);
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function () {
+
+        this.reply('hello world');
+    }
+});
+
+// Start the server
+server.start();
