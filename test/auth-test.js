6484be41f2cb9e493d7f6cd87e78d26b2ba6ed0b
Christopher Bebry <invalidsyntax@gmail.com>
2013-06-03 22:36:07 -0400

    Added travis stuff.

--- test/auth-test.js
+++ test/auth-test.js
@@ -5,26 +5,28 @@ describe('Auth', function() {
     describe('#generateNewHash(),#compareHashToPlaintext()', function() {
         it('should create a hash from a plaintext password and a salt, and verify one way.', function(done) {
             var auth = require("../lib/auth");
-			var inputPassword = "myPassword01";
-			
-			auth.generateNewHash(inputPassword, function(err, result) {
-				var salt = result.salt;
-				var hash = result.hash;
-				
-				console.log(salt.length)
-				assert(salt.length == 29); //expected length for right now
-				
-				auth.compareHashToPlaintext(inputPassword, hash, function(err, matchA) {
-					assert(matchA == true);
-					auth.compareHashToPlaintext("notMyPassword", hash, function(err, matchB) {
-						assert(matchB == false);
-						auth.compareHashToPlaintext(hash, inputPassword, function(err, matchC) {
-							assert(matchC == false);
-							done();
-						});
-					});
-				});
-			});
+            var inputPassword = "myPassword01";
+            
+            auth.generateNewHash(inputPassword, function(err, result) {
+                var salt = result.salt;
+                var hash = result.hash;
+                
+                console.log(salt.length);
+                console.log(hash.length);
+                assert(salt.length == 29); //expected length for right now
+                assert(hash.length == 60); //expected length for right now
+                
+                auth.compareHashToPlaintext(inputPassword, hash, function(err, matchA) {
+                    assert(matchA == true);
+                    auth.compareHashToPlaintext("notMyPassword", hash, function(err, matchB) {
+                        assert(matchB == false);
+                        auth.compareHashToPlaintext(hash, inputPassword, function(err, matchC) {
+                            assert(matchC == false);
+                            done();
+                        });
+                    });
+                });
+            });
         });
     });
 });
